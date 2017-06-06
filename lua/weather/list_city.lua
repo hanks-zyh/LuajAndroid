4ac8029cca5887e5a13b8170f36e43650f004330
kiya-z <orange.zjkele@gmail.com>
2017-06-07 00:22:11 +0800

    search city in weather

--- lua/weather/list_city.lua
+++ lua/weather/list_city.lua
@@ -3,21 +3,85 @@ require("import")
 import "android.widget.*"
 import "android.content.*"
 import "android.view.View"
+import "androlua.LuaAdapter"
 
 local layout = {
-  TextView,
-  id = "content",
-  layout_width = "match",
+    LinearLayout,
+    layout_width = "match",
+    layout_height = "match",
+    orientation = 'vertical',
+    fitsSystemWindows = true,
+     {
+        TextView,
+        layout_height = "56dp",
+        layout_width = "match",
+        gravity = "center",
+        text = "添加城市",
+        textSize = "18sp",
+        textColor = "#666666",
+    },
+    {
+        EditText,
+        id = 'key',
+        paddingLeft = "16dp",
+        layout_width = "match",
+        layout_height = "56dp",
+        hint = "搜索城市",
+    },
+    {
+        ListView,
+        id = 'listview',
+        layout_width = "match",
+        layout_height = "match",
+    }
 }
 
 
 local China  = require("weather.city")
 
+local item_view = {
+    TextView,
+    id = "text",
+    gravity = "center_vertical",
+    layout_widht = "fill",
+    layout_height = "56dp",
+    paddingLeft = "16dp",
+    textColor = "#666666",
+}
+
+local data = {}
+
 function onCreate(savedInstanceState)
-    activity.setStatusBarColor(0x00000000)
+    activity.setLightStatusBar()
     activity.setContentView(loadlayout(layout))
-    local text = ''
-    for k,v in pairs(China) do
-        text  = text .. ',' .. k
+    
+    for k,_ in pairs(China) do
+        data[#data + 1] = k
     end
+
+    local adapter = LuaAdapter(luajava.createProxy("androlua.LuaAdapter$AdapterCreator", {
+        getCount = function() return #data end,
+        getItem = function(position) return nil end,
+        getItemId = function(position) return position end,
+        getView = function(position, convertView, parent)
+            position = position + 1 -- lua 索引从 1开始
+            if convertView == nil then
+                local views = {} -- store views
+                convertView = loadlayout(item_view, views, ListView)
+                convertView.getLayoutParams().width = parent.getWidth()
+                convertView.setTag(views)
+            end
+            local views = convertView.getTag()
+            local item = data[position]
+            print(position,item)
+            views.text.setText(item)
+            return convertView
+        end
+    }))
+    listview.setAdapter(adapter)
+    listview.setOnItemClickListener(luajava.createProxy("android.widget.AdapterView$OnItemClickListener",{
+        onItemClick = function(adapter,view,position,id)
+            activity.toast('click item:' .. position)
+        end,
+    }))
 end
